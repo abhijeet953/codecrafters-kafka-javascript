0603531a75c64834a207e3598cad30942a19d60b
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2024-09-15 15:39:17 +0530

    Trying to handle APIVersion Request t14

--- app/main.js
+++ app/main.js
@@ -19,7 +19,7 @@ const server = net.createServer((connection) => {
     //Message Length
     connection.write(messageLength);
     //Correlation ID
-    connection.write(data.subarray(8,10));
+    connection.write(data.subarray(8,16));
     //No error
     let errorCode = Buffer.alloc(1);
     errorCode.writeUInt8(0);
