6fcc0291a9cd3f92898c0582d3a18d2b0870ada1
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2024-09-15 16:51:04 +0530

    Trying to handle APIVersion Request t26

--- app/main.js
+++ app/main.js
@@ -9,6 +9,7 @@ const server = net.createServer((connection) => {
     let apiKey = data.subarray(4, 6).readUInt16BE();
     let apiVersion = data.subarray(6, 8).readUInt16BE();
     let correlationID = data.subarray(8, 12).readUInt32BE();
+    let correlationIDString = correlationID.toString();
     console.log(apiKey)
     console.log(apiVersion);
     console.log(correlationID);
@@ -16,14 +17,14 @@ const server = net.createServer((connection) => {
     switch (apiKey) {
       case 18:
         if (apiVersion < 0 || apiVersion > 4) {
-          connection.write(correlationID);
+          connection.write(correlationIDString);
           let errorCode = Buffer.alloc(2);
           errorCode.writeUInt16BE(35);
           connection.write(errorCode);
         }
         
       default:
-        connection.write(correlationID);
+        connection.write(correlationIDString);
         let errorCode = Buffer.alloc(2);
         errorCode.writeUInt16BE(-1);
         connection.write(errorCode);
