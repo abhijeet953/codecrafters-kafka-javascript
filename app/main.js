2c2802c1bcc6945d80bdad4f42015db2c2567a66
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2024-09-15 16:49:13 +0530

    Trying to handle APIVersion Request t25

--- app/main.js
+++ app/main.js
@@ -1,19 +1,35 @@
-import net from  "net";
+import net from "net";
 
-const server = net.createServer((connection)=>{
+const server = net.createServer((connection) => {
 
-  connection.on('data',(data)=>{
+  connection.on('data', (data) => {
     console.log(data);
-    let length = data.subarray(0,4).readUInt32BE();
+    let length = data.subarray(0, 4).readUInt32BE();
     console.log(length);
-    let apiKey  = data.subarray(4,6).readUInt16BE();
-    let apiKeyResponse = data.subarray(6,8).readUInt16BE();
-    let correlationID = data.subarray(8,12).readUInt32BE();
+    let apiKey = data.subarray(4, 6).readUInt16BE();
+    let apiVersion = data.subarray(6, 8).readUInt16BE();
+    let correlationID = data.subarray(8, 12).readUInt32BE();
     console.log(apiKey)
-    console.log(apiKeyResponse);
-    console.log(correlationID)
+    console.log(apiVersion);
+    console.log(correlationID);
+
+    switch (apiKey) {
+      case 18:
+        if (apiVersion < 0 || apiVersion > 4) {
+          connection.write(correlationID);
+          let errorCode = Buffer.alloc(2);
+          errorCode.writeUInt16BE(35);
+          connection.write(errorCode);
+        }
+        
+      default:
+        connection.write(correlationID);
+        let errorCode = Buffer.alloc(2);
+        errorCode.writeUInt16BE(-1);
+        connection.write(errorCode);
+    }
 
   });
 });
 
-server.listen(9092,"127.0.0.1")
+server.listen(9092, "127.0.0.1")
