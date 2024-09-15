b8c70c650b9977412ad13b86efffe4986d88fe2b
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2024-09-15 16:37:47 +0530

    Trying to handle APIVersion Request t24

--- app/main.js
+++ app/main.js
@@ -3,9 +3,16 @@ import net from  "net";
 const server = net.createServer((connection)=>{
 
   connection.on('data',(data)=>{
-
+    console.log(data);
     let length = data.subarray(0,4).readUInt32BE();
     console.log(length);
+    let apiKey  = data.subarray(4,6).readUInt16BE();
+    let apiKeyResponse = data.subarray(6,8).readUInt16BE();
+    let correlationID = data.subarray(8,12).readUInt32BE();
+    console.log(apiKey)
+    console.log(apiKeyResponse);
+    console.log(correlationID)
+
   });
 });
 
