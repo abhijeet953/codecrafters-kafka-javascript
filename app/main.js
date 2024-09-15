57a52a9cf817eebffac34c6d2d465bdece62eb7c
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2024-09-15 19:28:24 +0530

    Include Fetch 1

--- app/main.js
+++ app/main.js
@@ -5,10 +5,64 @@ const server = net.createServer((conn) => {
   conn.on('data', (data) => {
 
     console.log(data);
+    let apiVersion = data.subarray(6, 8).readUInt16BE(0);
+    let correlationId_bytes = data.subarray(8, 12);
 
-    const correlationId_bytes = data.subarray(8,12);
-
+    console.log(apiVersion);
     console.log(correlationId_bytes);
+
+    const error_code_bytes = Buffer.alloc(2);
+    error_code_bytes.writeInt16BE(0, 0);
+
+    const api_key_bytes = Buffer.alloc(1);
+    api_key_bytes.writeInt8(2, 0);
+
+    const response_api_key_bytes = Buffer.alloc(2);
+    response_api_key_bytes.writeInt16BE(18, 0);
+
+    const api_key_min_version_bytes = Buffer.alloc(2);
+    api_key_min_version_bytes.writeInt16BE(4, 0);
+
+    const api_key_max_version_bytes = Buffer.alloc(2);
+    api_key_max_version_bytes.writeInt16BE(4, 0);
+
+    const tag_buffer_bytes = Buffer.alloc(2);
+    tag_buffer_bytes.writeInt16BE(0, 0);
+
+    const throttle_time_ms_bytes = Buffer.alloc(4);
+    throttle_time_ms_bytes.writeInt32BE(0, 0);
+
+    let msg_length = (correlationId_Bytes.length
+      + error_code_bytes.length
+      + api_key_bytes.length
+      + response_api_key_bytes.length
+      + api_key_min_version_bytes.length
+      + api_key_max_version_bytes.length
+      + tag_buffer_bytes.length
+      + throttle_time_ms_bytes.length);
+
+    console.log('Message Length:', msg_length);
+
+    const mlb = Buffer.alloc(4);
+    mlb.writeUInt32BE(msg_length, 0);
+
+    conn.write(mlb);
+    conn.write(correlationId_Bytes);
+    if (apiVersion >= 0 && apiVersion <= 4) {
+      console.log("INSIDE");
+      conn.write(error_code_bytes);
+    } else {
+      let errorCode = Buffer.alloc(2);
+      errorCode.writeUInt16BE(35);
+      conn.write(errorCode);
+    }
+
+    conn.write(api_key_bytes);
+    conn.write(response_api_key_bytes);
+    conn.write(api_key_min_version_bytes);
+    conn.write(api_key_max_version_bytes);
+    conn.write(tag_buffer_bytes);
+    conn.write(throttle_time_ms_bytes);
 
   });
 });
