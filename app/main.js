22472eecce8dbfe4eb7a59fb9e90d60f94e617df
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2024-09-15 18:45:43 +0530

    Handle APIVersions Request 17

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ const server = net.createServer((conn) => {
     console.log(data);
 
     let api_version_bytes = data.subarray(6,8).readUInt16BE(0);
-    let correlation_bytes = data.subarray(8,12).readUInt16BE(0);
+    let correlation_bytes = data.subarray(8,12).readUInt32BE(0);
 
     console.log(api_version_bytes);
     console.log(correlation_bytes);
