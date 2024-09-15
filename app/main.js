5a3836701d57ada59a3b834bfc536de65b377830
abhijeet953 <chowdhuryabhijeet98@gmail.com>
2024-09-15 14:40:53 +0530

    Parse-correlationID

--- app/main.js
+++ app/main.js
@@ -6,9 +6,16 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  const coorelationID = 7;
-  const response = Buffer.from([0,0,0,0,0,0,0,coorelationID]);
-  connection.write(response);
+  //Now we have to extract the correlation ID from the 
+  //incoming request
+
+  connection.on("data",(data)=>{
+    connection.write(data.subarray(4,12));
+  });
+
+  // const coorelationID = 7;
+  // const response = Buffer.from([0,0,0,0,0,0,0,coorelationID]);
+  // connection.write(response);
 });
 
 server.listen(9092, "127.0.0.1");
